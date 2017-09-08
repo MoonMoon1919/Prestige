452a7fd05cb9a1877e2ba6316ae518623f320ccd
Max Moon <moon.maxwell@gmail.com>
2017-09-08 16:30:08 -0700

    Adding in check s3 method
    Checks to see if files have been successfully upload to s3

--- prestige/prestige.py
+++ prestige/prestige.py
@@ -1,9 +1,13 @@
+#Get external libraries
 import boto3
 import sys
 import json
 import zipfile
 import os
+import datetime
 from botocore.exceptions import ClientError
+
+#Get internal libraries
 import prestige.config as config
 from prestige.cli import ARGS as args
 
@@ -34,8 +38,49 @@ def upload_files():
                       )
               for obj in upload:
                 print("Uploading file %s" % shortened_img_path)
-            except ClientError:
-              print("balls")
+            except ClientError as error:
+              print(error.response)
+
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
+
+def check_upload():
+  bucket = args.bucket
+  current_dir = os.curdir
+  walker = os.walk(current_dir)
+  length = len(current_dir)
+
+  to_upload = []
+
+  for root, folders, files in walker:
+    for file_name in files:
+      if file_name != 0:
+        shortened_img_path = os.path.join(root[length:], file_name)
+        to_upload.append(shortened_img_path)
+
+  current_objs = []
+
+  try:
+    current_objects = client.list_objects(
+                        Bucket='mm-img-bucket-test'
+                      )
+    dump_response = json.dumps(current_objects, default=datetime_handler, indent=4)
+    load_response = json.loads(dump_response)
+    objects = load_response['Contents']
+
+    for obj in objects:
+      current_objs.append(obj['Key'])  
+  except ClientError:
+    print(error.response)
+
+  for obj in to_upload:
+   if obj in current_objs:
+    print("Files uploaded to S3 successfully")
+    return True
+  else:
+    return False
 
 def get_urls():
   print("Getting urls")
@@ -48,10 +93,14 @@ def main():
     if args.optimize:
       photo_optim()
       upload_files()
-      get_urls()
+      if check_upload():
+        get_urls()
+      else:
+        print("Files not uploaded successfully")
     else:
       upload_files()
-      get_urls()
+      if check_upload():
+        get_urls()
 
 if __name__ == "__main__":
   main()
